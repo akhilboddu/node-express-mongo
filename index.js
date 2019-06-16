cc8cf5f41e435f4db9725d38bc10b7c20e44c9dc
Akhil Boddu <akhil1234mara@gmail.com>
2019-06-16 16:53:03 +0200

    added more CORs fix

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`)
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next()
 })
 app.use(todoRoute)

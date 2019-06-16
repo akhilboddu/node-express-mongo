b23d7ff4335aa27114e17fcfa5de9efc2214549e
Akhil Boddu <akhil1234mara@gmail.com>
2019-06-16 16:42:02 +0200

    Added cors code

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
 })
 app.use(todoRoute)
